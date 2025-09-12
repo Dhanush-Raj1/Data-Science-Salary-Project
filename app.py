c74e1e96711fc87eb2d0bc2055c1516815c6c01c
Dhanush <dhanush1004raj@gmail.com>
2025-09-12 19:21:49 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,57 +1,54 @@
+import sys 
 from src.logger import logging 
-from src.pipelines.predict_pipeline import Predict, NewData
+from src.exception_handling import Custom_Exception
+from src.pipelines.predict_pipeline1 import PredictData, NewData 
+from flask import Flask, request, render_template, url_for 
+import numpy as np
 
-from flask import Flask, request, render_template, url_for
+
 
 app = Flask(__name__)
 
-
-# route for home page
+# route for home page 
 @app.route('/')
-def home():
+def home(): 
     return render_template('home_page.html')
+ 
 
-
-# route for predict page
+# route for predict page 
 @app.route('/predictdata', methods=['GET', 'POST'])
-def predict():
-    if request.method == 'GET': 
-        return render_template('predict_page.html')
-     
+def predict_datapoint():
+    if request.method == 'GET':
+        return render_template('predict_page1.html')
     else:
-        logging.info("POST request has been made.")
-        data = NewData(Industry = request.form.get("Industry"), 
-                       Sector = request.form.get("Sector"), 
-                       Size = request.form.get("Size"),
-                       Revenue = request.form.get("Revenue"),
-                       Ownership = request.form.get("Ownership"),
-                       Rating = int(request.form.get("Rating")),  
-                       age = int(request.form.get("age")),
-                       python = int(request.form.get("python")),
-                       spark = int(request.form.get("spark")),
-                       aws = int(request.form.get("aws")),
-                       excel = int(request.form.get("excel")),
-                       LLMs = int(request.form.get("LLMs")),
-                       sql = int(request.form.get("sql")),
-                       Job_simp = request.form.get("Job_simp"),
-                       seniority = request.form.get("seniority") ) 
+        try:
+            logging.info("POST request has been made.")
+
+            data = NewData(
+                 Company_Rating = float(request.form.get("Company_Rating")),
+                 Skills = request.form.get("Skills"),
+                 Seniority = request.form.get('Seniority'),
+                 Job_type = request.form.get('Job_type'),
+                 Salary_Source = request.form.get('Salary_Source'),
+                 Location = request.form.get('Location') 
+            )
+
+            df = data.get_data_as_dataframe()
+            logging.info("Data has been converted into a dataframe: \n{df}")
+
+            model = PredictData()
+            result = model.predict_data(df)
+            logging.info(f"Prediction Result: {result}")
+
+            # Format the result 
+            predicted_salary = round(float(result[0]))
+            result_text =  "Your estimated salary is"
+
+            return render_template('predict_page1.html', result=predicted_salary, result_text=result_text)
+
+        except Exception as e:
+            raise Custom_Exception(e, sys)
         
-        df = data.get_data_as_dataframe()
-        logging.info("Data has been converted into a dataframe.")
-        logging.info(f"Dataframe: \n{df.head()}")
-        print(df.head())
-        
-        Prediction = Predict()
-        result = Prediction.predict_data(df)
-        logging.info("Prediction Result: {result}")
-        print(f"Predition result {result}")
-        
-        result_text =  "Your estimated salary is:"
-        
-        
-        return render_template('predict_page.html', result = result[0], result_text = result_text)
-        
-        
-        
+
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False)
+    app.run(debug=True, use_reloader=False, port=8000)
